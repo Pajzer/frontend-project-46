82aae984e8cb580be04ac6369048861da48f583a
Максим Лысов <pajzer@gmail.com>
2024-09-21 12:43:39 +0400

    update function index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import fs from 'fs';
 import path from 'path';
 import parse from './parses.js';
-
-const extractFormat = (filepath) => path.extname(filepath).slice(1);
+import getComparison from './getComparison.js';
 
 const getParseFile = (filePath) => {
     const fullPath = path.resolve(process.cwd(), filePath);
     const data = fs.readFileSync(fullPath, 'utf8');
-    const format = extractFormat(fullPath);
+    const format = path.extname(fullPath).slice(1);
     return parse(data, format);
 };
 
 const genDiff = (filepath1, filepath2) => {
     const data1 = getParseFile(filepath1);
     const data2 = getParseFile(filepath2);
-    console.log(data1);
-    console.log(data2);
+    return getComparison(data1, data2);
 };
 
 export default genDiff;
