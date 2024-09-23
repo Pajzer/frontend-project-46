75cf0198133ce06def18fbd53289c4a998e424dc
Максим Лысов <pajzer@gmail.com>
2024-09-23 11:46:04 +0400

    add yml test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,8 +8,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('compare two flat files', () => {
+test('compare two json files', () => {
   const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const excpectedResult = readFile('comparison-json.txt');
-  expect(result).toEqual(excpectedResult);
+  const expectedResult = readFile('expected _result.txt');
+  expect(result).toEqual(expectedResult);
+});
+
+test('compare two yaml files', () => {
+  const result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+  const expectedResult = readFile('expected _result.txt');
+  expect(result).toEqual(expectedResult);
 });
