17d338102a82828365e18554dd4b6016db1910bd
Максим Лысов <pajzer@gmail.com>
2024-09-24 11:26:00 +0400

    rename function

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const getComparison = (obj1, obj2) => {
+const buildTree = (obj1, obj2) => {
   const key1 = _.keys(obj1);
   const key2 = _.keys(obj2);
   const sortedKeys = _.sortBy(_.union(key1, key2));
@@ -8,7 +8,7 @@ const getComparison = (obj1, obj2) => {
   return sortedKeys.map((key) => {
     if (!_.has(obj1, key)) { return { key, type: 'added', value: obj2[key] }; }
     if (!_.has(obj2, key)) { return { key, type: 'deleted', value: obj1[key] }; }
-    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) { return { key, type: 'nested', children: getComparison(obj1[key], obj2[key]) }; }
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) { return { key, type: 'nested', children: buildTree(obj1[key], obj2[key]) }; }
     if (!_.isEqual(obj1[key], obj2[key])) {
       return {
         key, type: 'changed', value1: obj1[key], value2: obj2[key],
@@ -19,4 +19,4 @@ const getComparison = (obj1, obj2) => {
   });
 };
 
-export default getComparison;
+export default buildTree;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import getComparison from './getComparison.js';
+import buildTree from './buildTree.js';
 import parse from './parses.js';
 import getReport from './formatters/index.js';
 
@@ -14,7 +14,7 @@ const getData = (filepath) => {
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = getData(filepath1);
   const data2 = getData(filepath2);
-  const tree = getComparison(data1, data2);
+  const tree = buildTree(data1, data2);
   return getReport(tree, format);
 };
 
